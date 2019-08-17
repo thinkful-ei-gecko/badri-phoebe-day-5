51af6fd51a5a1207b69092bf74e09d6a69896592
Badri-narayan <badritulsiram@gmail.com>
2019-08-17 09:55:17 -0700

    added some functions

--- datastore.js
+++ datastore.js
@@ -3,7 +3,7 @@ const STORE = [
     question: 'What is my child doing?',
     answers: ['Playing games on the computer', 'Delaying adulthood', 'Learning how to use the Google', 'Learning part time', 'Spending 40-60 hours weekly in an intensive program to become a junior developer in five months!'],
     correctAnswer: 'Spending 40-60 hours weekly in an intensive program to become a junior developer in five months!',
-    fact: '<Growth of development as a career field>'
+    fact: 'Growth of development as a career field'
   },
   {
     question: 'Is this profitable?',
@@ -14,13 +14,13 @@ const STORE = [
   {
     question: 'What if it\'s not best for my child (and they decide to pursue something else)?',
     answers: ['My child will continue to delay adulthood.', 'My child will pursue their dream in becoming a cowboy/cowgirl astronaut racecar driver.', 'My child will switch to being an Uber driver.', '<Something about Thinkful offering a refund>'],
-    correctAnswer: '<Something about Thinkful offering a refund>',
-    fact: '<something about the refund and prep course if they try decide not to do it>'
+    correctAnswer: 'Something about Thinkful offering a refund',
+    fact: 'something about the refund and prep course if they try decide not to do it'
   },
   {
     question: 'Will my child get a job afterward?',
     answers: ['Yes, but not as a web/software developer.', 'No', 'Yes, Thinkful guarantees that qualifying graduates get a job in their field six months after graduating.', 'What is a job?'],
-    correctAnswer: 'Yes, Thinkful guarantees that qualifying graduates get a job in their field six months after graduating.'
+    correctAnswer: 'Yes, Thinkful guarantees that qualifying graduates get a job in their field six months after graduating.',
     fact: `If you don't get a job within that time, they will <a href="https://tf-assets-prod.s3.amazonaws.com/shoebill/PDFs/thinkful-job-guarantee.pdf">refund the cost of tuition</a>.`
   },
   {
@@ -29,7 +29,7 @@ const STORE = [
       console.log('I love you parents!')c
     }`,
     answers: ['What the heck is a for loop?','For me?', 'For ever', '(let i = 0, i <= 3, i++)'],
-    correctAnswer: '(let i = 0, i <= 3, i++)'
+    correctAnswer: '(let i = 0, i <= 3, i++)',
     fact: 'A "for loop" is used to tell the code how many times to repeat itself!' 
   }
 ]
--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 let questionNumber = 0;
 let score = 0;
 
+
 //get the start page
 function renderStartPage () {
-  console.log('hello');
   $('.js-quiz-box').html(`
-    <h1>How Much Do You Know About Your Child's Bootcamp?</h1>
-    <p>Yikes! Has your child signed up for a "coding bootcamp"? Have no fear, the internet is here!</p>
-    <p>If your child is now in Thinkful's Engineering Immersion program, this brief quiz will check your knowledge on just what your child has gotten into and what they are learning. You may even learn a thing or two about it yourself!</p>
+    <h1 class = "quizTitle">How Much Do You Know About Your Child's Bootcamp?</h1>
+    <p class = "quizText">Yikes! Has your child signed up for a "coding bootcamp"? Have no fear, the internet is here!</p>
+    <p class = "quizText">If your child is now in Thinkful's Engineering Immersion program, this brief quiz will check your knowledge on just what your child has gotten into and what they are learning. You may even learn a thing or two about it yourself!</p>
     <button class="startButton">Start the Quiz</button>
     <p id="disclaimer">Disclaimer: This quiz was made by a Thinkful student to fulfill a quiz app project requirement. While the statistics used in the quiz are true, this in no way reflects the views or opinions of Thinkful staff, students, or Thinkful as a whole. Apart from fulfilling course requirements, the developer (me) intended to use it to show off to her parents in a cheeky way and help them understand more of what she's doing. :)</p>
   `);
@@ -19,25 +19,25 @@ function renderStartPage () {
 // get the questions and results
 function generateQuestion (){
   if (questionNumber < STORE.length) {
-    return `<div class="js-quiz-box">
-      <h1>Question 1</h1>
+    return `<div>
+      <h1>${STORE[questionNumber].question} </h1>
       <form class="questionForm">
         <fieldset>
           <label class="answerOption">
             <input type="radio" value="Answer 1" name="answer" required>
-            <span>Answer 1</span>
+            <span>${STORE[questionNumber].answers[0]}</span>
           </label>    
           <label class="answerOption">
             <input type="radio" value="Answer 2" name="answer" required>
-            <span>Answer 2</span>
+            <span>${STORE[questionNumber].answers[1]}</span>
           </label>
           <label class="answerOption">
             <input type="radio" value="Answer 3" name="answer" required>
-            <span>Answer 3</span>
+            <span>${STORE[questionNumber].answers[2]}</span>
           </label>    
           <label class="answerOption">
             <input type="radio" value="Answer 4" name="answer" required>
-            <span>Answer 4</span>
+            <span>${STORE[questionNumber].answers[3]}</span>
           </label>
           <button type="submit" class="submitButton">Submit</button>
         </fieldset>
@@ -46,8 +46,45 @@ function generateQuestion (){
   }
 }
 
+function renderQuestion() {
+  $('.js-quiz-box').html(generateQuestion);
+}
+
+function renderResultsView() {
+  if (userInput === STORE[questionNumber].correctAnswer){
+    $('.js-quiz-box').html(`
+    <div>
+      <h1> 🎉 Hooray!! 🎉</h1>
+        <p> You got it right! Good job. 🍪</p>
+        <p> ${STORE[questionNumber].fact} </p>
+      <button class="next-question">Next</button>
+    </div>
+  `)
+  }
+  else {
+    $('.js-quiz-box').html(`
+    <div> 
+      <h1> 😭😭😭 </h1>
+        <p> Sorry! That wasn't quite right... </p>
+        <p> The correct answer is: ${STORE[questionNumber].correctAnswer} </p>
+      <button class="next-question">Next</button>
+    </div>
+    `)
+  }
+}
+
+function renderEndPage() {
+  $('.js-quiz-box').html(`
+  <div> 
+    <h1> Not bad... </h1>
+      <p> Final Score: ${score}/${STORE.length} </p>
+    <button class="restart">Try Again?</button> 
+  </div>
+  `)
+}
+
 function createQuiz () {
-  renderStartPage();
+  renderEndPage();
 }
 
 $(createQuiz);
