26bc763a0a5176bc5c77b7b49614722dafcdaba2
Phoebe Law <phoebebasilio@gmail.com>
2019-08-18 18:29:34 -0700

    Removed event listener functions from createQuiz() so they will run with renderStartPage()

--- index.js
+++ index.js
@@ -13,12 +13,9 @@ function renderStartPage () {
     <p class = "quizText">Yikes! Has your child signed up for a "coding bootcamp"? Have no fear, the internet is here!</p>
     <p class = "quizText">If your child is now in Thinkful's Engineering Immersion program, this brief quiz will check your knowledge on just what your child has gotten into and what they are learning. You may even learn a thing or two about it yourself!</p>
     <button type="button" class="next-question">Start the Quiz</button>
-    <p id="disclaimer">Disclaimer: This quiz was made by a Thinkful student to fulfill a quiz app project requirement. While the statistics used in the quiz are true, this in no way reflects the views or opinions of Thinkful staff, students, or Thinkful as a whole. Apart from fulfilling course requirements, the developer (me) intended to use it to show off to her parents in a cheeky way and help them understand more of what she's doing. :)</p>
+    <p id="disclaimer">Disclaimer: This quiz was made by Thinkful students to fulfill a quiz app project requirement. While the statistics used in the quiz are true, this in no way reflects the views or opinions of Thinkful staff, students, or Thinkful as a whole. Apart from fulfilling course requirements, it was created to show off their new skills to their parents in a cheeky way and help them understand more of the course itself. :)</p>
   `);
 }
-
-//Renders next question
-
 
 // get the questions and results
 function generateQuestion (){
@@ -93,41 +90,36 @@ function renderEndPage() {
   <div> 
     <h1> Not bad... </h1>
       <p> Final Score: ${score}/${STORE.length} </p>
-    <button class="restart">Try Again?</button> 
+    <button type="button" class="restart">Try Again?</button> 
   </div>
   `)
 }
 
-function createQuiz () {
-  console.log('quiz created');
+//Display next question on "next question clicked"
+$('.js-quiz-box').on('click', '.next-question', event => {
+  $('.js-quiz-box').empty();
+  if (questionNumber === 5) {
+    renderEndPage();
+  } else {
+    renderQuestion();
+  }
+});
+  
+//Display result on "submit" click
+$('form').on('submit', event => {
+  event.preventDefault();
+  userInput = $('input:checked').val();
+  $('.js-quiz-box').empty();
+  generateResultsView();
+});
+  
+//Restart quiz button (refresh question number, score, userInput and run createQuiz())
+$('.js-quiz-box').on('click', '.restart', event => {
+  $('.js-quiz-box').empty();
+  questionNumber = 1;
+  score = 0;
+  userInput = '';
   renderStartPage();
-  
-  //Display next question on "next question clicked"
-  $('.js-quiz-box').on('click', '.next-question', event => {
-    $('.js-quiz-box').empty();
-    if (questionNumber === 5) {
-      renderEndPage();
-    } else {
-      renderQuestion();
-    }
-  });
-  
-  //Display result on "submit" click
-  $('form').on('submit', event => {
-    event.preventDefault();
-    userInput = $('input:checked').val();
-    $('.js-quiz-box').empty();
-    generateResultsView();
-  });
-  
-  //Restart quiz button (refresh question number, score, userInput and run createQuiz())
-  $('.js-quiz-box').on('click', '.restart', event => {
-    $('.js-quiz-box').empty();
-    questionNumber = 1;
-    score = 0;
-    userInput = '';
-    renderStartPage();
-  });
-}
+});
 
 $(renderStartPage);
