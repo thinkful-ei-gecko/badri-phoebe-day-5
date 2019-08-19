4299266dc84848c450c89a81077817c1b74af87a
Phoebe Law <phoebebasilio@gmail.com>
2019-08-18 17:34:43 -0700

    Added functionality for buttons, in progress

--- index.js
+++ index.js
@@ -3,18 +3,22 @@
 
 let questionNumber = 0;
 let score = 0;
-
+let userInput = '';
 
 //get the start page
 function renderStartPage () {
+  console.log('rendered start page');
   $('.js-quiz-box').html(`
     <h1 class = "quizTitle">How Much Do You Know About Your Child's Bootcamp?</h1>
     <p class = "quizText">Yikes! Has your child signed up for a "coding bootcamp"? Have no fear, the internet is here!</p>
     <p class = "quizText">If your child is now in Thinkful's Engineering Immersion program, this brief quiz will check your knowledge on just what your child has gotten into and what they are learning. You may even learn a thing or two about it yourself!</p>
-    <button class="startButton">Start the Quiz</button>
+    <button type="button" class="next-question">Start the Quiz</button>
     <p id="disclaimer">Disclaimer: This quiz was made by a Thinkful student to fulfill a quiz app project requirement. While the statistics used in the quiz are true, this in no way reflects the views or opinions of Thinkful staff, students, or Thinkful as a whole. Apart from fulfilling course requirements, the developer (me) intended to use it to show off to her parents in a cheeky way and help them understand more of what she's doing. :)</p>
   `);
 }
+
+//Renders next question
+
 
 // get the questions and results
 function generateQuestion (){
@@ -39,7 +43,7 @@ function generateQuestion (){
             <input type="radio" value="Answer 4" name="answer" required>
             <span>${STORE[questionNumber].answers[3]}</span>
           </label>
-          <button type="submit" class="submitButton">Submit</button>
+          <button type="submit" class="submit-button">Submit</button>
         </fieldset>
       </form>
     </div>`
@@ -47,30 +51,38 @@ function generateQuestion (){
 }
 
 function renderQuestion() {
+  console.log('test');
   $('.js-quiz-box').html(generateQuestion);
 }
 
-function renderResultsView() {
-  if (userInput === STORE[questionNumber].correctAnswer){
-    $('.js-quiz-box').html(`
+function generateResultsView() {
+  if (userInput === STORE[questionNumber].correctAnswer) {
+    renderCorrectResultsView();
+  } else {
+    renderIncorrectResultsView();
+  }
+}
+
+function renderCorrectResultsView() {
+  $('.js-quiz-box').html(`
     <div>
       <h1> 🎉 Hooray!! 🎉</h1>
-        <p> You got it right! Good job. 🍪</p>
-        <p> ${STORE[questionNumber].fact} </p>
-      <button class="next-question">Next</button>
+      <p> You got it right! Good job. 🍪</p>
+      <p> ${STORE[questionNumber].fact} </p>
+      <button type="button" class="next-question">Next</button>
     </div>
   `)
-  }
-  else {
+}
+
+function renderIncorrectResultsView() {
     $('.js-quiz-box').html(`
     <div> 
       <h1> 😭😭😭 </h1>
         <p> Sorry! That wasn't quite right... </p>
         <p> The correct answer is: ${STORE[questionNumber].correctAnswer} </p>
-      <button class="next-question">Next</button>
+      <button type="button" class="next-question">Next</button>
     </div>
     `)
-  }
 }
 
 function renderEndPage() {
@@ -83,8 +95,23 @@ function renderEndPage() {
   `)
 }
 
+
+
 function createQuiz () {
-  renderQuestion();
+  console.log('quiz created');
+  renderStartPage();
+  
+  $('form').on('submit', event => {
+    event.preventDefault();
+    userInput = $('input:checked').val();
+    $('.js-quiz-box').empty();
+    generateResultsView();
+  });
+  
+  $('.js-quiz-box').on('click', '.next-question', event => {
+    $('.js-quiz-box').empty();
+    renderQuestion();
+  });
 }
 
 $(createQuiz);
