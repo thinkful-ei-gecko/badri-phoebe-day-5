1dba7b46e333df582537358d0f23c15bbc4ff05b
Badri-narayan <badritulsiram@gmail.com>
2019-08-19 10:36:20 -0700

    small changes to datastore

--- datastore.js
+++ datastore.js
@@ -27,8 +27,8 @@ const STORE = [
   },
   {
     question: `Complete this for loop by filling in the blank so that it prints the message three times: 
-    <code>for ________________ {
-      console.log('I love you parents!</code>')
+    <br> <code>for ________________ { <br>
+      console.log('I love you parents!</code>') <br>
     }`,
     answers: ['What the heck is a for loop?','For me?', 'For ever', '<code>(let i = 0, i <= 3, i++)</code>'],
     correctAnswer: '<code>(let i = 0, i <= 3, i++)</code>',
